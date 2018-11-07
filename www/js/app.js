182baf7cfdfe2c0ad4256c490ae6a629656e7447
André Bonfante <andre458bonfante@gmail.com>
2018-11-07 21:52:39 -0200

    Criação do repositório

---
+++ www/js/app.js
@@ -0,0 +1,12 @@
+angular.module('todo', ['ionic'])
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (window.cordova && window.Keyboard) {
+      window.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+
+})
